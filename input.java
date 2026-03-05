d719d4d985070b88d0ee5343df157baf43d2ba65
Vaishnavi Ananda Ayarekar <vaishnaviayarekar22@gmail.com>
2026-03-05 22:58:55 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class input
+{
+public static void main(String[] args)
+{
+
+   Scanner sc= new Scanner(System.in);
+   System.out.println("Eneter the number :");
+   int num=sc.nextInt();
+   System.out.println("The number is " + num );
+  }
+}
+
+
